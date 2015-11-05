24c7c33684b4bbfe1752737f2f086a2ee852a21c
Ashok Borra <coded9@users.noreply.github.com>
2015-11-05 11:51:22 +0530

    Create Meet Bonfire.js

---
+++ Bonfires/Basic-Algorithm-Scripting/Meet Bonfire.js
@@ -0,0 +1,14 @@
+/* Bonfire: Meet Bonfire
+  Code by Ashok
+  http://www.freecodecamp.com/coded9
+*/
+function meetBonfire(argument) {
+  // Good luck!
+  console.log("you can read this function's argument in the developer tools", argument);
+
+  return true;
+}
+
+
+
+meetBonfire("You can do this!");
