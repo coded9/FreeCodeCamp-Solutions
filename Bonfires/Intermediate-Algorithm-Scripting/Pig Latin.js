e860e7bf306702bb0824fee9a8835cff02fe6984
Ashok Borra <coded9@users.noreply.github.com>
2015-11-15 15:35:08 -0600

    Create Pig Latin.js

---
+++ Bonfires/Intermediate-Algorithm-Scripting/Pig Latin.js
@@ -0,0 +1,18 @@
+/*
+Bonfire: Pig Latin
+Author:Ashok
+*/
+function translate(str) {
+  var regex =/[aeiou]/gi;
+  if(regex.test(str[0])){
+    str = str+'way';
+  }
+  else{
+    i = str.indexOf(str.match(regex)[0]);
+    
+  str = str.slice(i)+str.substr(0,i)+'ay';
+  }
+ return str;
+}
+
+translate("consonant");
