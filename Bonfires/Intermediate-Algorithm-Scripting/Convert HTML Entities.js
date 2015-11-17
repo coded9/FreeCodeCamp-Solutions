9e0ba52ca98344e828ea13f88edd4641bdfad7b2
Ashok Borra <coded9@users.noreply.github.com>
2015-11-17 14:45:44 -0600

    New method added

--- Bonfires/Intermediate-Algorithm-Scripting/Convert HTML Entities.js
+++ Bonfires/Intermediate-Algorithm-Scripting/Convert HTML Entities.js
@@ -2,6 +2,7 @@
 Bonfire: Convert HTML Entities
 Author:Ashok
 */
+Method1 :
 function convert(str) {
   // &colon;&rpar;
 var replaceObj ={
@@ -21,3 +22,14 @@ return str;
 
 
 convert("Dolce & Gabbana");
+
+Method2:
+function convert(str) {
+  str = str.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;').replace(/'/g,'&apos;');
+return str;
+}
+
+
+convert("Dolce & Gabbana");
+
+
