80e53ee41109b01fad79be4efcd1c66f0cf7f828
Ashok Borra <coded9@users.noreply.github.com>
2015-11-18 13:53:11 -0600

    Problem finished
    Atlast solved one of the difficult challenges.

--- Bonfires/Intermediate-Algorithm-Scripting/Where art thou.js
+++ Bonfires/Intermediate-Algorithm-Scripting/Where art thou.js
@@ -3,14 +3,28 @@ Bonfire: Where art thou
 
 */
 function where(collection, source) {
-  var arr = [];
-  // What's in a name?
-  for(a in collection){
-    if(collection[a].hasOwnProperty(source) && collection[a][source] = source ){
-      arr.push(a);
-    }
-  }
-  return arr;
+  var arr = []; //For storing the matched objects
+  var count ; //For maintaining number of key-value matches
+  function hasValue(obj, key, value) {
+    if( obj.hasOwnProperty(key) && obj[key] === value)
+      {
+        count++; //If the value is matched increase the count
+      }
+}
+  
+
+   collection.some(function(collObj) { //Some function for iterating through all objects in Collection
+     count = 0 ; //Initialize the count for each and every object to 0
+       for(var prop in source){
+         a = source[prop];
+         hasValue(collObj,prop,a); //To check the key-value pair match
+       }
+      if(count>=Object.keys(source).length){ //If the count is equal or more,add the object to the array object
+        arr.push(collObj);
+      }
+    });
+   return arr;
 }
 
-where([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+//where([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+where([{ "a": 1, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
