547862e731688991fb4ae7f320d6e0701c9e7f86
Ashok Borra <coded9@users.noreply.github.com>
2015-11-22 00:44:05 -0600

    New Solution added

--- Bonfires/Intermediate-Algorithm-Scripting/Diff Two Arrays.js
+++ Bonfires/Intermediate-Algorithm-Scripting/Diff Two Arrays.js
@@ -19,3 +19,13 @@ function diff(arr1, arr2) {
 }
 //diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 diff(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
+
+function diff(arr1,arr2){
+  var newArr = [];
+  newArr = arr1.filter(function(i){
+    return arr2.indexOf(i) === -1;
+  }).concat(arr2.filter(function(i){
+    return arr1.indexOf(i) === -1;
+  }));
+  return newArr;
+}
