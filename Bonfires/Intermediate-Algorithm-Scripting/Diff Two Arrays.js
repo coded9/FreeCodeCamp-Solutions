c394c6de5edb2c9c7ef5c4d6165757786cf6f5f6
Ashok Borra <coded9@users.noreply.github.com>
2015-11-22 00:39:09 -0600

    Create Diff Two Arrays.js

---
+++ Bonfires/Intermediate-Algorithm-Scripting/Diff Two Arrays.js
@@ -0,0 +1,21 @@
+/*
+Bonfire: Diff Two Arrays
+Author:Ashok
+*/
+function diff(arr1, arr2) {
+  var newArr = [];
+  // Same, same; but different.
+  for(i=0;i<arr1.length;i++){
+    if(arr2.indexOf(arr1[i]) === -1){
+       newArr.push(arr1[i]);
+      }
+  }
+   for(i=0;i<arr2.length;i++){
+    if(arr1.indexOf(arr2[i]) === -1){
+        newArr.push(arr2[i]);
+      }
+  }
+  return newArr;
+}
+//diff([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+diff(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]);
