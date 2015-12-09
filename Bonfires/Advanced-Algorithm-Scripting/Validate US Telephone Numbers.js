9eb57324bf9e22641a42c75309764c1f78a2d19c
Ashok Borra <coded9@users.noreply.github.com>
2015-12-08 20:56:29 -0600

    Update Validate US Telephone Numbers.js

--- Bonfires/Advanced-Algorithm-Scripting/Validate US Telephone Numbers.js
+++ Bonfires/Advanced-Algorithm-Scripting/Validate US Telephone Numbers.js
@@ -1,7 +1,18 @@
 function telephoneCheck(str) {
   // Good luck!
   str = str.replace(/[^0-9]/g,"");
- return str;
+  Num = parseInt(str);
+ if(typeof Num== "number" && str.length>=10 ){
+   if(str.length==11 &&str.match(/^1/)==1){
+   return true;
+   }
+   else if(str.length == 10){
+     return true;
+   }
+   else
+     return false;
+ }
+  return false;
   //return true;
 }
 
