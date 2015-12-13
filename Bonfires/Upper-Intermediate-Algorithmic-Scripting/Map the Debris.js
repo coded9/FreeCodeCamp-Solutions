10340adabe395a333313ac00a659d89f5800efc7
Ashok Borra <coded9@users.noreply.github.com>
2015-12-13 15:19:14 +0530

    Update Map the Debris.js

--- Bonfires/Upper-Intermediate-Algorithmic-Scripting/Map the Debris.js
+++ Bonfires/Upper-Intermediate-Algorithmic-Scripting/Map the Debris.js
@@ -15,3 +15,18 @@ function orbitalPeriod(arr) {
 }
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  console.log(arr);
+  for(var prop in arr){
+  var orbitalPer = Math.round(2*Math.PI*Math.sqrt(Math.pow(arr[prop].avgAlt+earthRadius,3)/GM));
+  delete arr[prop].avgAlt;
+  arr[prop].orbitalPeriod = orbitalPer;
+  console.log(arr);
+  }
+  return arr;
+}
+
+orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
