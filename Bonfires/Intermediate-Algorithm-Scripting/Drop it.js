b7fb01584abf4870eb3ea5009f104f28138d3932
Ashok Borra <coded9@users.noreply.github.com>
2015-12-13 18:26:26 -0600

    Update Drop it.js

--- Bonfires/Intermediate-Algorithm-Scripting/Drop it.js
+++ Bonfires/Intermediate-Algorithm-Scripting/Drop it.js
@@ -2,3 +2,18 @@
 Bonfire: Drop it
 Author:Ashok
 */
+function drop(arr, func) {
+  // Drop them elements.
+ var len = arr.length;
+  for(i=0;i<len;i++){
+    if(func(arr[0])){
+      
+      break;
+    }
+     else
+        arr.shift();
+  }
+  return arr;
+}
+
+drop([1, 2, 3, 7, 4], function(n) {return n > 3;});
