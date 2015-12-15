ff2d569611fcd3c753e859000e1ffcac5d23fd1e
Ashok Borra <coded9@users.noreply.github.com>
2015-12-15 03:28:43 +0200

    Temporary solution

--- Bonfires/Advanced-Algorithm-Scripting/No repeats please.js
+++ Bonfires/Advanced-Algorithm-Scripting/No repeats please.js
@@ -1,3 +1,42 @@
 /*
 Bonfire: No repeats please
 */
+function permAlone(str) {
+  function permutations(str){
+var arr = str.split(''),
+    len = arr.length, 
+    perms = [],
+    rest,
+    picked,
+    restPerms,
+    next;
+
+    if (len === 0)
+        return [str];
+
+    for (var i=0; i<len; i++)
+    {
+        rest = Object.create(arr);
+        picked = rest.splice(i, 1);
+
+        restPerms = permutations(rest.join(''));
+
+       for (var j=0, jLen = restPerms.length; j< jLen; j++)
+       {
+           next = picked.concat(restPerms[j]);
+           perms.push(next.join(''));
+       }
+    }
+
+   return perms;
+   
+}
+  function filterPerms(str){
+  var re = /(.)\1+/g;
+  
+  return !Boolean(str.match(re));
+}
+  return permutations(str).filter(filterPerms).length;
+}
+
+permAlone('abab');
